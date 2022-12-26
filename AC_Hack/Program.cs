8f8d736aae2c68b03582c9f85b0a7d3e1bd54975
Snee <120136010+itssnee@users.noreply.github.com>
2022-12-26 13:53:13 +0100

    Add files via upload

---
+++ AC_Hack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AC_Hack
+{
+    class Program
+    {
+        public static int Base = 0x004E4DBC;
+        public static int Health = 0xF4;
+
+        static void Main(string[] args)
+        {
+            VAMemory vam = new VAMemory("ac_client");
+
+            int LocalPlayer = vam.ReadInt32((IntPtr)Base);
+            int LocalPlayer2 = vam.ReadInt32((IntPtr)Base);
+
+            Console.Title = "https://github.com/itssnee";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Assault Cube hack v1");
+            Console.ResetColor();
+            while (true)
+            {
+                int address = LocalPlayer + Health;
+                vam.WriteInt32((IntPtr)address, 9999);
+
+                Thread.Sleep(100);
+
+
+            }
+
+
+
+        }
+    }
+}
